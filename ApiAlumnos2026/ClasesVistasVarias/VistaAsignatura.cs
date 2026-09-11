c4568a47615d9746792d2db72e5dba1d60800151
VGiaroli <valegiaroli2016@gmail.com>
2026-09-11 09:39:12 -0300

    Limpieza: borrado de subcarpeta duplicada

--- ApiAlumnos2026/ClasesVistasVarias/VistaAsignatura.cs
+++
@@ -1,9 +0,0 @@
-namespace ApiAlumnos2026.ClasesVistasVarias
-{
-    public class VistaAsignatura
-    {
-        public int AsignaturaId { get; set; }
-        public string? Descripcion { get; set; }
-        public bool Eliminado { get; set; }
-    }
-}
--- ApiAlumnos2026/ClasesVistasVarias/VistaPromedioAsignatura.cs
+++
@@ -1,9 +0,0 @@
-using ApiAlumnos2026.ClasesVistasVarias;
-
-public class VistaPromedioAsignatura
-{
-    public int AsignaturaId {get; set;}
-    public string Descripcion {get; set;}
-
-    public decimal Promedio { get; set; } 
-}
